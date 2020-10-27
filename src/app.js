bee2b4b396a4b74fd6bd1cf7b100990f117163b4
an vo <>
2020-10-27 20:53:36 +0700

    notification

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 import 'react-native-gesture-handler';
 
 import AppNavigator from './app-navigator';
@@ -10,8 +11,24 @@ const styles = StyleSheet.create({
   },
 });
 
-export default () => (
-  <SafeAreaView style={styles.container}>
-    <AppNavigator />
-  </SafeAreaView>
-);
+async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+  }
+}
+
+export default () => {
+  useEffect(() => {
+    requestUserPermission();
+  }, []);
+  return (
+    <SafeAreaView style={styles.container}>
+      <AppNavigator />
+    </SafeAreaView>
+  );
+};
