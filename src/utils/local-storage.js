f8495012476bf18fad9a121d4a2e86332618e824
An Vo <anvotrinh@gmail.com>
2020-12-18 00:00:34 +0700

    prepare for the change

--- src/utils/local-storage.js
+++ src/utils/local-storage.js
@@ -4,9 +4,15 @@ export const TOKEN = 'token'
 export const WATERMARK = 'watermark'
 
 export const getLocalItem = async key => {
-  return await AsyncStorage.getItem(key)
+  const itemStr = await AsyncStorage.getItem(key)
+  try {
+    return JSON.parse(itemStr)
+  } catch (e) {
+    return itemStr
+  }
 }
 
-export const saveLocalItem = async (key, token) => {
-  return await AsyncStorage.setItem(key, token)
+export const saveLocalItem = async (key, item) => {
+  const itemStr = JSON.stringify(item)
+  return await AsyncStorage.setItem(key, itemStr)
 }
